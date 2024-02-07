86f25a47946014e3fb89990a9685fb870e395070
imkuromi <156067407+imkuromi@users.noreply.github.com>
2024-02-07 22:59:23 +0700

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:onboarding_screen/screen/introduction_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+bool show = true;
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final prefs = await SharedPreferences.getInstance();
+  show = prefs.getBool('ON_BORDING') ?? true;
+  runApp(const MyApp());
+  }
+
+  class MyApp extends StatelessWidget {
+    const MyApp({super.key});
+  
+    @override
+    Widget build(BuildContext context) {
+      return MaterialApp(
+        title: 'Onboarding Screen',
+        theme: ThemeData(
+          primaryColor: Colors.green,
+        ),
+        home: IntroScreen(),
+      );
+    }
+  }
